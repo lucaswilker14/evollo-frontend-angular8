9d9a89ce086f91b62244f64ff52e5223b28fba9c
Lucas Wilker <lwilker24@gmail.com>
2021-03-03 20:32:05 -0300

    Organizing Routes

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,11 @@
 import {RouterModule, Routes} from '@angular/router';
-import {AppComponent} from './app.component';
 import {HomePageComponent} from './home-page/home-page.component';
 import {ModuleWithProviders} from '@angular/core';
+import {LoginComponent} from './login/login.component';
 
 const APP_ROUTES: Routes = [
-    {path: '/', component: AppComponent},
-    {path: '/home', component: HomePageComponent}
-]
+    {path: '', component: LoginComponent},
+    {path: 'home', component: HomePageComponent}
+];
 
 export const routing: ModuleWithProviders<any> = RouterModule.forRoot(APP_ROUTES);
