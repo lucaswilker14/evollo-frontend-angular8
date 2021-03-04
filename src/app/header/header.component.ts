5885b41da8128bba932069033bd5024a1b4d505a
Lucas Wilker <lwilker24@gmail.com>
2021-03-04 17:52:58 -0300

    WIP - Get api to add new company

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
     selector: 'app-header',
@@ -9,11 +10,16 @@ export class HeaderComponent implements OnInit {
 
     @Input() title: string;
 
-    constructor() {
+    constructor(private router: Router) {
     }
 
     ngOnInit(): void {
 
+    }
+
+    logout = () => {
+        localStorage.clear();
+        this.router.navigate(['']);
     }
 
 
