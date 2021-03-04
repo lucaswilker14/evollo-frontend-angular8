1945824c44fae4b315e113ffd8f72a8f36cfcbf0
Lucas Wilker <lwilker24@gmail.com>
2021-03-04 14:17:54 -0300

    created view

--- src/app/register-employee/register-employee.component.ts
+++ src/app/register-employee/register-employee.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
     selector: 'app-register-employee',
@@ -7,10 +8,17 @@ import {Component, OnInit} from '@angular/core';
 })
 export class RegisterEmployeeComponent implements OnInit {
 
-    constructor() {
+    constructor(private router: Router) {
     }
 
     ngOnInit(): void {
+    }
+
+    goBack = () => {
+        this.router.navigate(['/home']);
+    }
+
+    registerUser = () => {
     }
 
 }
