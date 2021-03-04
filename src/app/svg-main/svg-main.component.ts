545ef49bff460eff54dac383bf6e2784d782ec05
Lucas Wilker <lwilker24@gmail.com>
2021-03-04 13:46:46 -0300

    Created SVG Component

---
+++ src/app/svg-main/svg-main.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-svg-main',
+  templateUrl: './svg-main.component.html',
+  styleUrls: ['./svg-main.component.css']
+})
+export class SvgMainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
