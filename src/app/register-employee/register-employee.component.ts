1e3d61c13d55e6917c63e1f1ac68b7141724908a
Lucas Wilker <lwilker24@gmail.com>
2021-03-04 22:06:24 -0300

    Finish Register Employee Component

--- src/app/register-employee/register-employee.component.ts
+++ src/app/register-employee/register-employee.component.ts
@@ -1,5 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, Renderer2, ViewChild} from '@angular/core';
 import {Router} from '@angular/router';
+import {RegisterEmployeeService} from './register-employee.service';
+import {ToastrService} from 'ngx-toastr';
 
 @Component({
     selector: 'app-register-employee',
@@ -8,7 +10,24 @@ import {Router} from '@angular/router';
 })
 export class RegisterEmployeeComponent implements OnInit {
 
-    constructor(private router: Router) {
+    @ViewChild('formControl') formControlHtml: ElementRef;
+
+    registerEmployee = {
+        name: '',
+        email: '',
+        cpf: '',
+        jobRole: '',
+        salary: '',
+        permission: ''
+    };
+
+    optionPermissions = [
+        {name: 'User', value: 'USER'},
+        {name: 'Admin', value: 'ADMIN'}
+    ];
+
+    constructor(private router: Router, private renderer: Renderer2,
+                private registerEmployeeService: RegisterEmployeeService, private toast: ToastrService) {
     }
 
     ngOnInit(): void {
@@ -18,7 +37,27 @@ export class RegisterEmployeeComponent implements OnInit {
         this.router.navigate(['/home']);
     }
 
-    registerUser = () => {
+    registerUser = (event) => {
+        this.validatedFormInputs(event);
+        this.registerEmployeeService.registerEmployee(this.registerEmployee)
+            .subscribe(data => {
+                    this.toast.success('Funcionário Registrado com Sucesso!');
+                    this.toast.info('Email com credenciais enviado para o email ' + this.registerEmployee.email);
+                    this.router.navigate(['home']);
+                },
+                error => {
+                    this.toast.error('Não foi possivel cadastrar funcionário');
+                }
+            );
+    }
+
+    validatedFormInputs = (event) => {
+        const InputFormControl = this.formControlHtml.nativeElement[0];
+        if (InputFormControl.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+        this.renderer.addClass(this.formControlHtml.nativeElement, 'was-validated');
     }
 
 }
